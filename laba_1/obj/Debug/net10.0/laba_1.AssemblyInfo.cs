135ae9bc44b6713d6a450e215a08d2dbe43dfa1f
artem priutesa <priutesaa8@gmail.com>
2026-03-03 17:30:09 +0200

    все готово

--- laba_1/obj/Debug/net10.0/laba_1.AssemblyInfo.cs
+++ laba_1/obj/Debug/net10.0/laba_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("laba_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a63267afdbe0b5bcf5380ce32f5a3e5ff5f5308c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e0b599ec1ec2d7060079069a90dacdd68648bca")]
 [assembly: System.Reflection.AssemblyProductAttribute("laba_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("laba_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
